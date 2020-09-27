89c2c2b1a291ee9d48a85f2bb90c0e96acf851cc
Administrator <Administrator@DESKTOP-0J08NVI>
2020-09-27 19:06:05 +0800

    JDBC单元测试完成

--- jUnitTest/dd/junit4/JDBCTest.java
+++ jUnitTest/dd/junit4/JDBCTest.java
@@ -2,13 +2,25 @@ package dd.junit4;
 
 import static org.junit.Assert.*;
 
+import java.sql.ResultSet;
+import java.util.List;
+import java.util.Map;
+
 import org.junit.Test;
+
+import dd.utils.JDBCUtils;
 
 public class JDBCTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test() throws Exception {
+		String sql = "select *from student";
+		
+		List<Map<String, Object>> resultSet = JDBCUtils.QueryBySQL(sql);
+		System.out.println("数据库中学生姓名依次为:");
+		for (Map<String, Object> map : resultSet) {
+			System.out.println(map.get("name"));
+		}
 	}
 
 }
